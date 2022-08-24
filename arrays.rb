7177f73b366dc578e36255896e5b37c140387e5b
J Seymour <JustJakeSeymour@gmail.com>
2022-08-24 11:31:47 -0600

    Add content to arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+#variable with 4+ Strings
+nhl_teams = ["Avalanche", "Kings", "Coyotes", "Flames"]
+#variable with 4+ Integers
+games_won = [56, 44, 25, 50]
+#variable with 4+ Floats
+points_per_game = [1.45, 1.21, .69, 1.35]
+#variable with 4+ Booleans
+playoffs = [true, true, false, true]
+ 
